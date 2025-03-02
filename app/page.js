d1339bdf0cd3ce2e0eb559a0930f980ee4bafa39
VP-29 <164088719+VP-29@users.noreply.github.com>
2025-03-01 18:38:40 -0700

    Adding initial main page

---
+++ app/page.js
@@ -0,0 +1,28 @@
+'use client'; 
+import { useState } from "react";
+import Navbar from '../app/components/Navbar'; 
+import StudentList from '../app/components/Studentlist';
+import students from '../public/students.json'; 
+import Footer from '../app/components/Footer';
+import AddStudentForm from '../app/components/AddStudentForm';
+
+export default function Home() {
+  const [studentsList, setStudentsList] = useState(students);
+
+  const addStudent = (newStudent) => {
+    setStudentsList([...studentsList, { ...newStudent, id: studentsList.length + 1 }]);
+  };
+
+  return (
+    <div className="container mx-auto p-4">
+      <Navbar />
+      <div className="flex flex-col md:flex-row gap-6">
+        <div className="w-full md:w-2/3">
+          <StudentList students={studentsList} />
+        </div>
+        <AddStudentForm onAddStudent={addStudent} />
+      </div>
+      <Footer />
+    </div>
+  );
+}
